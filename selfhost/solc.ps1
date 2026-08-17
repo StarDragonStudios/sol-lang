01424b99795fc3d7595ef3a9f70af188705d67c6
rodrigo <rodrigomurillo666@hotmail.com>
2026-08-17 21:39:00 +0200

    fix(selfhost): stream Windows requests as UTF-8 bytes

--- selfhost/solc.ps1
+++ selfhost/solc.ps1
@@ -121,16 +121,14 @@ try {
     $ProcessInfo.RedirectStandardInput = $true
     $ProcessInfo.RedirectStandardOutput = $true
     $ProcessInfo.RedirectStandardError = $true
-    $Utf8NoBom = [Text.UTF8Encoding]::new($false)
-    $ProcessInfo.StandardInputEncoding = $Utf8NoBom
-    $ProcessInfo.StandardOutputEncoding = $Utf8NoBom
-    $ProcessInfo.StandardErrorEncoding = $Utf8NoBom
     $Process = [Diagnostics.Process]::new()
     $Process.StartInfo = $ProcessInfo
     if (-not $Process.Start()) { throw "self-host compiler core did not start" }
     $StandardOutput = $Process.StandardOutput.ReadToEndAsync()
     $StandardError = $Process.StandardError.ReadToEndAsync()
-    $Process.StandardInput.WriteLine($Request)
+    $RequestLine = [Text.UTF8Encoding]::new($false).GetBytes("$Request`n")
+    $Process.StandardInput.BaseStream.Write($RequestLine, 0, $RequestLine.Length)
+    $Process.StandardInput.BaseStream.Flush()
     $Process.StandardInput.Close()
     $Process.WaitForExit()
     [Console]::Error.Write($StandardOutput.Result)
